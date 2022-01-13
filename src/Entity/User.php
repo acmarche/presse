6080617f4f06553610c1281e8d7a00a6eeef629d
jfsenechal <jfsenechal@gmail.com>
2022-01-13 12:29:39 +0100

    fix auth

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,16 +2,18 @@
 
 namespace AcMarche\Presse\Entity;
 
-use AcMarche\Presse\Repository\UserRepository;
-use Stringable;
 use AcMarche\Presse\Doctrine\IdEntityTrait;
+use AcMarche\Presse\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Stringable;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements UserInterface, Stringable
+class User implements UserInterface, Stringable, PasswordAuthenticatedUserInterface
 {
     use IdEntityTrait;
+
     #[ORM\Column(type: 'string', length: 180, unique: true)]
     private ?string $username = null;
     #[ORM\Column(type: 'string', length: 100)]
@@ -27,24 +29,29 @@ class User implements UserInterface, Stringable
     private ?string $nom = null;
     #[ORM\Column(type: 'string', length: 100, nullable: true)]
     private ?string $prenom = null;
+
     public function __toString(): string
     {
-        return (string) $this->username;
+        return (string)$this->username;
     }
+
     public function getUserIdentifier(): string
     {
         return $this->username;
     }
+
     public function getUsername(): ?string
     {
         return $this->username;
     }
+
     public function setUsername(string $username): self
     {
         $this->username = $username;
 
         return $this;
     }
+
     public function getRoles(): array
     {
         $roles = $this->roles;
@@ -53,6 +60,7 @@ class User implements UserInterface, Stringable
 
         return array_unique($roles);
     }
+
     public function addRole(string $role): self
     {
         if (!in_array($role, $this->roles, true)) {
@@ -61,6 +69,7 @@ class User implements UserInterface, Stringable
 
         return $this;
     }
+
     public function removeRole(string $role): self
     {
         if (in_array($role, $this->roles, true)) {
@@ -70,26 +79,31 @@ class User implements UserInterface, Stringable
 
         return $this;
     }
+
     public function hasRole(string $role): bool
     {
         return in_array($role, $this->getRoles(), true);
     }
+
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
 
         return $this;
     }
+
     public function getPassword(): ?string
     {
         return $this->password;
     }
+
     public function setPassword(string $password): self
     {
         $this->password = $password;
 
         return $this;
     }
+
     /**
      * @see UserInterface
      */
@@ -97,6 +111,7 @@ class User implements UserInterface, Stringable
     {
         // not needed when using the "bcrypt" algorithm in security.yaml
     }
+
     /**
      * @see UserInterface
      */
@@ -105,30 +120,36 @@ class User implements UserInterface, Stringable
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
     public function getNom(): ?string
     {
         return $this->nom;
     }
+
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
 
         return $this;
     }
+
     public function getPrenom(): ?string
     {
         return $this->prenom;
     }
+
     public function setPrenom(string $prenom): self
     {
         $this->prenom = $prenom;
 
         return $this;
     }
+
     public function getEmail(): ?string
     {
         return $this->email;
     }
+
     public function setEmail(string $email): self
     {
         $this->email = $email;
--- src/Security/AppPresseAuthenticator.php
+++ src/Security/AppPresseAuthenticator.php
@@ -11,11 +11,9 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class AppPresseAuthenticator extends AbstractLoginFormAuthenticator
@@ -30,7 +28,7 @@ class AppPresseAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email', '');
+        $email = $request->request->get('username', '');
         $password = $request->request->get('password', '');
         $token = $request->request->get('_csrf_token', '');
 
@@ -39,7 +37,6 @@ class AppPresseAuthenticator extends AbstractLoginFormAuthenticator
         $badges =
             [
                 new CsrfTokenBadge('authenticate', $token),
-                new PasswordUpgradeBadge($password, $this->userRepository),
             ];
 
         return new Passport(
@@ -54,7 +51,7 @@ class AppPresseAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl(Request $request): string

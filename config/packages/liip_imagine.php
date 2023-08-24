05e2e87702a4eac6f6b89f03e0fc6c46fa3272b6
jfsenechal <jfsenechal@gmail.com>
2023-08-24 09:27:46 +0200

    up sf 6.3

--- config/packages/liip_imagine.php
+++ config/packages/liip_imagine.php
@@ -1,25 +1,68 @@
 <?php
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\LiipImagineConfig;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->extension('liip_imagine', ['resolvers' => ['default' => ['web_path' => null]]]);
+
     $containerConfigurator->extension(
         'liip_imagine',
         [
-            'driver' => 'gd',
-            'resolvers' => ['default' => ['web_path' => null]],
             'filter_sets' => [
                 'cache' => null,
-                'my_thumb' => [
-                    'jpeg_quality' => 100,
-                    'filters' => ['thumbnail' => ['size' => [120, 45], 'mode' => 'inset']],
-                ],
                 'miniature' => [
-                    'jpeg_quality' => 100,
+                    'quality' => 100,
                     'filters' => ['thumbnail' => ['size' => [240, 180], 'mode' => 'outbound']],
                 ],
-                'my_heighten_filter' => ['filters' => ['relative_resize' => ['heighten' => 120]]],
+                'my_thumb' => [
+                    'quality' => 100,
+                    'filters' => ['thumbnail' => ['size' => [120, 45], 'mode' => 'inset']],
+                ],
+                'my_heighten_filter' => [
+                    'quality' => 100,
+                    'filters' => [
+                        'relative_resize' => [
+                            'heighten' => 120,
+                        ],
+                    ],
+                ],
             ],
-        ]
+        ],
     );
+};
+
+return static function (LiipImagineConfig $liipImagineConfig): void {
+
+    $liipImagineConfig
+        ->driver('gd')
+        ->resolvers('default', ['web_path' => []]);
+
+    $liipImagineConfig->filterSet('miniature', [
+        'jpeg_quality' => 100,
+        'filters' => [
+            'thumbnail' => [
+                'size' => '240, 180',
+                'mode' => 'outbound',
+            ],
+        ],
+    ]);
+
+    $liipImagineConfig->filterSet('my_thumb', [
+        'jpeg_quality' => 100,
+        'filters' => [
+            'thumbnail' => [
+                'size' => '120, 45',
+                'mode' => 'inset',
+            ],
+        ],
+    ]);
+
+    $liipImagineConfig->filterSet('my_heighten_filter', [
+        'filters' => [
+            'relative_resize' => [
+                'heighten' => 120,
+            ],
+        ],
+    ]);
 };

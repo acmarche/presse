0336e596dfb0a21b5dc7ef51060a4fe7c9e7c109
jfsenechal <jfsenechal@gmail.com>
2025-01-09 15:02:12 +0100

    fix null

--- src/Entity/Destinataire.php
+++ src/Entity/Destinataire.php
@@ -18,7 +18,7 @@ class Destinataire implements Stringable
     public ?string $prenom = null;
     #[ORM\Column(type: 'string', length: 100)]
     public ?string $email = null;
-    #[ORM\Column(type: 'string', length: 100)]
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
     public ?string $username = null;    
     #[ORM\Column(type: 'boolean')]
     public bool $attachment = false;

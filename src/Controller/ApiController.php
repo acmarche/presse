4ec28ba2aefce2902abffe483b5d89c1bb57f91d
jfsenechal <jfsenechal@gmail.com>
2025-02-19 10:10:38 +0100

    add api

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -19,7 +19,7 @@ class ApiController extends AbstractController
     #[Route(path: '/articles',methods: ['GET'])]
     public function index(): JsonResponse
     {
-        $articles = $this->articleRepository->findLast(10);
+        $articles = $this->articleRepository->findLast(8);
         $data = $this->presseService->serializeArticles($articles);
 
         return $this->json($data);
